d58e6292f731c2f5fabccc039534d6eb680bc5da
Sumit Patil <sumitpatil12309@gmail.com>
2026-08-02 15:01:29 +0530

    Create Oddnumber.cpp

---
+++ Oddnumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<=20;i++)
+    {
+        if(i%2!=0)
+        {
+        cout<<i<<"\n";
+    }
+}    
+    
+    return 0;
+}
