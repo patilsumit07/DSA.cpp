338cc5292e11ce1416fae72fa518e2490875ef68
Sumit Patil <sumitpatil12309@gmail.com>
2026-08-02 14:57:58 +0530

    Create Areaoftrianglle.cpp

---
+++ Areaoftrianglle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int base,hight;
+    float area;
+    cout<<"\nEnter a Base:";
+    cin>>base;
+    cout<<"\nEnter a Hight:";
+    cin>>hight;
+
+    area=0.5*base*hight;
+    cout<<"\nArea Of Triangle Is:"<<area;
+    return 0;
+}
