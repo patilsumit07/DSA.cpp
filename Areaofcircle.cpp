df006153f800f730724626a949aa8fcaddca225f
Sumit Patil <sumitpatil12309@gmail.com>
2026-08-02 14:56:45 +0530

    Create Areaofcircle.cpp

---
+++ Areaofcircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int radius,pi=3.14;
+    float area;
+    cout<<"\nEnter A Radius:";
+    cin>>radius;
+
+    area=radius*radius*pi;
+    cout<<"\nArea Of Circle Is:"<<area;
+    return 0;
+}
